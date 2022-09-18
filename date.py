532b740c475e4cb1c42e1acdf0519559011fb50f
Mohamed.ABDC <41598266+mohamedabcd@users.noreply.github.com>
2022-09-19 00:36:51 +0200

    Update date.py

--- date.py
+++ date.py
@@ -1,4 +1,34 @@
-from az.cli import az
+# app.py
+"""
+Demonstration/Example script of `az("<Command>")` interface
+"""
+from az.cli import az, SUCCESS_CODE
 
-print("hello")
-az("login")
+
+def main():
+    """
+    Demonstrage the usage of `az("<Command>")` by invoking one failing and one working cmd
+    """
+    show(*az("login"))
+
+    show(*az("group show -n test"))
+    show(*az("group list"))
+
+
+def show(exit_code, result_dict, log):
+    """
+    Prints a successfull command using pprint
+    Prints an unsuccessfull command as a simple print
+    """
+    import pprint
+    pp = pprint.PrettyPrinter(indent=2)
+
+    if exit_code == SUCCESS_CODE:
+        pp.pprint(result_dict)
+    else:
+        print(log)
+
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
